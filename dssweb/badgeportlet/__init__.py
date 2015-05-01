97eff2f28f9dc6323d219ba7ac6f95686b2a774e
Carol McMasters-Stone <cbeck@ucdavis.edu>
2015-05-01 10:16:30 -0700

    removed permissions - portlet not showing up

--- dssweb/badgeportlet/__init__.py
+++ dssweb/badgeportlet/__init__.py
@@ -3,8 +3,3 @@ PloneMessageFactory = MessageFactory('plone')
 
 from Products.CMFCore.permissions import setDefaultRoles
 
-PROJECTNAME = "dssweb.badgeportlet"
-DEFAULT_ADD_CONTENT_PERMISSION = "%s: Add badge portlet" % PROJECTNAME
-
-setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION,
-                ('Manager', 'Site Administrator', 'Owner',))
